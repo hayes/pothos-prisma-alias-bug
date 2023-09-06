1dcac144348513d3320edd23551ed4d44f78db20
Michael Hayes <michael.hayes@mystenlabs.com>
2023-09-06 11:58:38 -0700

    fix seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,16 +9,16 @@ faker.seed(123);
 
 async function main() {
   for (let i = 1; i <= 10; i += 1) {
-    const values: { date: Date; value: string }[] = [];
     const scores: { date: Date; score: string; name: string }[] = [];
+    const values: { date: Date; value: string; scores: { create: typeof scores } }[] = [];
 
-    const scoreNames = ["score1", "score2", "score3", "score4", "score5"];
+    const scoreNames = ['score1', 'score2', 'score3', 'score4', 'score5'];
 
     for (let j = 0; j < 100; j += 1) {
       scores.push({
         date: faker.date.past({ refDate: new Date(2021, 0, 1) }),
         score: faker.string.alpha(10),
-        name: scoreNames[(Math.random() * scoreNames.length) | 0]
+        name: scoreNames[(Math.random() * scoreNames.length) | 0],
       });
     }
 
@@ -28,7 +28,7 @@ async function main() {
         date: faker.date.past({ refDate: new Date(2021, 0, 1) }),
         scores: {
           create: scores,
-        }
+        },
       });
     }
 
